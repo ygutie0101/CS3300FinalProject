55af5cabaf16dc4fc7c36700d590a89e669411dd
ygutie0101 <ygutierr@uccs.edu>
2023-04-24 22:56:01 +0000

    Create app and add boostrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,8 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import "bootstrap"
+import "../stylesheets/application" //This is your stylesheet file where you will write all the styling code.
 
 Rails.start()
 Turbolinks.start()
