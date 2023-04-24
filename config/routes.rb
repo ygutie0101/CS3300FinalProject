ee82ca2b79873f3511b2de8535826a42e9aaa471
ygutie0101 <ygutierr@uccs.edu>
2023-04-24 22:34:12 +0000

    itration03 branch

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  resources :items
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # Define the root path routes
+  root "items#index"
+  resources :items
 end
